82b4f335a607cf66a8687fbee07c243c596108ca
YashBawa <ybawa100@gmail.com>
2023-02-13 09:49:51 +0530

    pretty basic

---
+++ codechef/practise/difficulty>1000/scholarship.cpp
@@ -0,0 +1,22 @@
+// How much Scholarship
+// Difficulty Rating:1012
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n ;
+    cin >> n ;
+    if (n<=50)
+    {
+        cout << 100 << endl;
+    }
+    else if (n<=100)
+    {
+        cout << 50 << endl;
+    }
+    else 
+    {
+        cout << 0 << endl;
+    }
+}
