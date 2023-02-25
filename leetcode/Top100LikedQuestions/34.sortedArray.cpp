029c4f52c5bd149cfd1384cb30c5b5a858f79b3b
YashBawa <ybawa100@gmail.com>
2023-02-25 22:23:56 +0530

    array--finding--position

---
+++ leetcode/Top100LikedQuestions/34.sortedArray.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        // for finding start index
+        vector<int> new_array;
+        int start =-1,end=-1;
+        for (int i=0;i<nums.size();i++)
+        {
+            if (nums[i] == target)
+            {
+                start = i;
+                break;
+            }
+        }
+        // for finding end index
+        for (int i=0;i<nums.size();i++)
+        {
+            if (nums[i] == target)
+            {
+                end = i;
+                
+            }
+        }
+        // array created values of start and end stored into it -->
+        new_array.push_back(start);
+        new_array.push_back(end);
+        
+        return new_array;
+    }
+};
