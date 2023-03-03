de4fbfbb5d4173b19e241c2d8504c49bef7efb5b
YashBawa <ybawa100@gmail.com>
2023-03-03 14:03:41 +0530

    streak

---
+++ codechef/practise/TAXES.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+
+    int t ;
+
+    cin >> t;
+
+    int x;
+
+    while (t--)
+
+    {
+
+        cin >> x;
+
+        if (x>100)
+
+        {
+
+            cout << x-10 << endl;
+
+        }
+
+        
+
+        else 
+
+        {
+
+            cout << x << endl;
+
+        }
+
+    }
+
+}
+
+
